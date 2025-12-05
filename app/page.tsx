7f1a81e90fe3338ba7bd4784ff0627127a8402e7
kc878 <kentcagadas19@gmail.com>
2025-12-05 16:18:10 +0800

    init: default dashboard ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p4 rounded-sm lg:col-span-2 p-2">Test</div>
-      <div className="bg-primary-foreground p4 rounded-sm p-2">Test</div>
-      <div className="bg-primary-foreground p4 rounded-sm p-2">Test</div>
-      <div className="bg-primary-foreground p4 rounded-sm p-2">Test</div>
-      <div className="bg-primary-foreground p4 rounded-sm p-2 lg:col-span-2">Test</div>
-      <div className="bg-primary-foreground p4 rounded-sm p-2">Test</div>
+    <div className="p-4 space-y-4">
+
+      {/* TOP GRID */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 auto-rows-fr">
+
+        {/* Left box */}
+        <div className="bg-primary-foreground rounded-sm p-2 min-h-[300px]">
+          Preview
+        </div>
+
+        {/* Right side grid */}
+        <div className="rounded-sm lg:col-span-3">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 auto-rows-fr">
+            <div className="bg-primary-foreground rounded-sm p-2 min-h-[250px]">Today</div>
+            <div className="bg-primary-foreground rounded-sm p-2 min-h-[250px]">Habits</div>
+            <div className="bg-primary-foreground rounded-sm p-2 min-h-[250px]">Tasks & Projects</div>
+            <div className="bg-primary-foreground rounded-sm p-2 min-h-[250px]">Metrics</div>
+          </div>
+        </div>
+
+      </div>
+
+      {/* BOTTOM GRID */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 auto-rows-fr">
+        <div className="bg-primary-foreground rounded-sm p-2 min-h-[300px]">Goals</div>
+        <div className="bg-primary-foreground rounded-sm p-2 min-h-[250px]">Goals Dashboard</div>
+        <div className="bg-primary-foreground rounded-sm p-2 min-h-[250px]">Life Metrics</div>
+      </div>
 
     </div>
   );
