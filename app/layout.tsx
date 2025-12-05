271d4f58fcafa65d0593529f62da2cf8db55c803
kc878 <kentcagadas19@gmail.com>
2025-12-05 14:39:49 +0800

    init: default dark mode theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,19 +26,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
-        <AppSidebar />
-        <main className="w-full">
-          <Navbar />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <AppSidebar />
+          <main className="w-full">
+            <Navbar />
 
-          {/*wrap children to give style to the content  */}
-          <div className="px-4">
-            {children}
-          </div>
-        </main>
+            {/*wrap children to give style to the content  */}
+            <div className="px-4">
+              {children}
+            </div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
-import { LogOut, Moon, Settings, User } from "lucide-react"
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 import {
@@ -11,23 +13,48 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-
+import { Button } from './ui/button'
+import { useTheme } from "next-themes"
 
 const DEFAULT_ITEM_STYLE = "h-[1.2rem] w-[1.2rem] m4-2";
 
 const Navbar = () => {
+
+  const { setTheme } = useTheme();
   return (
     <nav className="p-4 flex items-center justify-between">
       {/* LEFT */}
       collapseButton
 
       {/* RIGHT */}
-
       <div className="flex items-center gap-4">
         <Link href='/'>Dashboard</Link>
-        <Moon />
+
+        {/* THEME MENU */}
         <DropdownMenu>
-          <DropdownMenuTrigger>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* USER MENU */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
             <Avatar>
               <AvatarImage src="https://share.google/aQmKhlNYauPnw8vQs" />
               <AvatarFallback>LF</AvatarFallback>
